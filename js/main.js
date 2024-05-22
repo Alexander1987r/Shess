250ddc56cc6fc8ed6eb2c943d59aa70534848037
Alexander1987r <alexnext7771987r@rambler.ru>
2024-05-22 10:25:35 +0300

    add carousel

--- js/main.js
+++ js/main.js
@@ -1,34 +1,39 @@
 
 const swiper= new Swiper('.stages__list',{
   navigation:{
-      prevEl:'.swiper-button-prev',
-      nextEl:'.swiper-button-next'
+      prevEl:'.stages__list-button_prev',
+      nextEl:'.stages__list-button_next'
   },
   pagination:{
-      el:'.swiper-pagination',
+      el:'.stages__list-pagination',
       clickable:false,
   },
   breakpoints: {
-    // when window width is >= 320px
+
     1366: {enabled: false},
   },
 });
 
-const newSwiper= new Swiper('.list',{
+
+
+const newSwiper= new Swiper('.participants__list',{
   navigation:{
-    prevEl:'.swiper-button-prev',
-    nextEl:'.swiper-button-next'
+    prevEl:'.participants__list-button_prev',
+    nextEl:'.participants__list-button_next',
   },
   pagination:{
-    el:'.number-pagination',
+    el:'.participants__list-pagination',
     type:'fraction',
   },
   breakpoints: {
     768:{
       slidesPerView:2,
+      spaceBetween:20,
     },
     1366:{
       slidesPerView:3,
+      spaceBetween:20,
     },
   },
 });
+
